4e7a4178ce34d383f5f13fe2632241b51156607e
dev <dev@local>
2026-07-10 22:23:52 +0200

    Bump adl to v0.1.3

--- Formula/adl.rb
+++ Formula/adl.rb
@@ -4,10 +4,10 @@
 class Adl < Formula
   desc "Terminal download manager powered by aria2c"
   homepage "https://github.com/Flontistacks/adl"
-  url "https://github.com/Flontistacks/adl/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "cbacc6fb5e681b3748063c6c7eeff6ef22a445877a8ddcbaf356c24b2ec5cec0"
+  url "https://github.com/Flontistacks/adl/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "5ca5b32c805496f79b6ee170c83bd0cf8d9a031ca80a39f87a61ff00c8856f6a"
   license "MIT"
-  version "0.1.2"
+  version "0.1.3"
 
   head "https://github.com/Flontistacks/adl.git", branch: "main", using: :git
 
